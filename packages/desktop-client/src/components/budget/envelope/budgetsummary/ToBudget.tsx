01fe9cb9f02811f928f7825678aa44fd8e838905
UnderKoen <koenvanstaveren@hotmail.com>
2024-10-30 20:52:26 +0100

    enhance: context menu budget page positioning

--- packages/desktop-client/src/components/budget/envelope/budgetsummary/ToBudget.tsx
+++ packages/desktop-client/src/components/budget/envelope/budgetsummary/ToBudget.tsx
@@ -58,11 +58,20 @@ export function ToBudget({
   const isMenuOpen = Boolean(menuOpen);
   const contextMenusEnabled = useFeatureFlag('contextMenus');
 
+  const [crossOffset, setCrossOffset] = useState(0);
+  const [offset, setOffset] = useState(0);
+  const [contextMenuOpen, setContextMenuOpen] = useState(false);
+
   return (
     <>
       <View ref={triggerRef}>
         <ToBudgetAmount
-          onClick={() => setMenuOpen('actions')}
+          onClick={() => {
+            setOffset(0);
+            setCrossOffset(0);
+            setContextMenuOpen(false);
+            setMenuOpen('actions');
+          }}
           prevMonthName={prevMonthName}
           style={style}
           amountStyle={amountStyle}
@@ -71,17 +80,23 @@ export function ToBudget({
             if (!contextMenusEnabled) return;
             e.preventDefault();
             setMenuOpen('actions');
+            const rect = e.currentTarget.getBoundingClientRect();
+            setCrossOffset(e.clientX - rect.left);
+            setOffset(e.clientY - rect.bottom);
+            setContextMenuOpen(true);
           }}
         />
       </View>
 
       <Popover
         triggerRef={triggerRef}
-        placement="bottom"
+        placement={contextMenuOpen ? 'bottom start' : 'bottom'}
         isOpen={isMenuOpen}
         onOpenChange={() => setMenuOpen(null)}
-        style={{ width: 200 }}
+        style={{ width: 200, margin: 1 }}
         isNonModal
+        crossOffset={crossOffset}
+        offset={offset}
       >
         <span tabIndex={-1} ref={ref}>
           {menuOpen === 'actions' && (
